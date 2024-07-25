f59b4efe0d201422a0942acd1c07c3099b025637
XM7M <hamod977a@gmail.com>
2024-07-25 23:06:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World")
-})
+app.listen(process.env.PORT || 2000, () => {
+  console.log('Your app is listening on port ' + (process.env.PORT || 2000));
+  console.log("I'm Ready To Work..! 24H");
+});
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.send(`
+    <body>
+      <center><h1>Bot 24H ON!</h1></center>
+    </body>
+  `);
+});
+
 const { Client } = require('discord.js-selfbot-v13');
-const client = new Client({checkUpdate:false}); 
+const client = new Client(); // All partials are loaded automatically
 
-setInterval(() => {
-  if (!client || !client.user) {
-    console.log("Client not login");
-    console.log("Restart project");
-    process.kill(1);
-  }
-}, 1000);
-
-client.on("ready", async () => {
+client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
 });
-//ثبات فويس 24 ساعه v13 بدون اي مشاكل
-const { joinVoiceChannel } = require("@discordjs/voice");
-client.on("ready", () => {
+
+// ثبات فويس 24 ساعه v13 بدون اي مشاكل
+const { joinVoiceChannel } = require('@discordjs/voice');
+client.on('ready', () => {
   setInterval(async () => {
-    client.channels
-      .fetch(process.env.channel)
-      .then((channel) => {
-        const VoiceConnection = joinVoiceChannel({
-          channelId: channel.id,
-          guildId: process.env.guild,
-          selfMute: true,
-          selfDeaf: true,
-          adapterCreator: channel.guild.voiceAdapterCreator,
-        });
-      })
-      .catch((error) => {
-        return;
+    try {
+      const channel = await client.channels.fetch(process.env.channel);
+      const VoiceConnection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: process.env.guild,
+        selfMute: true,
+        selfDeaf: true,
+        adapterCreator: channel.guild.voiceAdapterCreator
       });
+    } catch (error) {
+      console.error('Error joining voice channel:', error);
+    }
   }, 1000);
 });
+
 client.login(process.env.token);
